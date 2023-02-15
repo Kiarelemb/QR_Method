33f9a367a8d171b09fa6fc4ebf56d07ac1f791b5
Kiarelemb QR <Kiarelemb@outlook.com>
2023-02-15 10:45:20 +0800

    upload

---
+++ src/method/qr/kiarelemb/file/action/QRFileReaderLineAction.java
@@ -0,0 +1,8 @@
+package method.qr.kiarelemb.file.action;
+
+public interface QRFileReaderLineAction {
+	/**
+	 * @param lineText 文本行内容
+	 */
+	void lineText(String lineText);
+}
---
+++ src/method/qr/kiarelemb/string/QRSplitStringUtils.java
@@ -0,0 +1,10 @@
+package method.qr.kiarelemb.string;
+
+/**
+ * @author Kiarelemb QR
+ * @program: QR_Method
+ * @description:
+ * @create 2023-02-15 10:13
+ **/
+public class QRSplitStringUtils {
+}
